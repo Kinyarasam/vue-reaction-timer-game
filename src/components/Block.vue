8d0b2108ddc88025011fea890258906f5f56d5cd
Kinyarasam <89331895+Kinyarasam@users.noreply.github.com>
2021-10-29 10:36:52 +0300

    Vue Reaction timer game.

---
+++ src/components/Block.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="block" v-if="showBlock" @click="stopTimer">
+        Click Me        
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0,
+        };
+    },
+    mounted() {
+    //   console.log('component mounted')
+      setTimeout(() => {
+        this.showBlock = true
+        this.startTimer()
+        // console.log(this.delay)
+      }, this.delay)
+    },
+   /*  updated() {
+      console.log('components updated')
+    },
+    unmounted() {
+      console.log('components unmounted')
+    }, */
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            console.log(this.reactionTime)
+            this.$emit('end', this.reactionTime);
+        },
+    }
+}
+</script>
+
+<style>
+body{ 
+    background: rgba(200,200,200,0.5)
+}
+.block {
+    width: 400px;
+    border-radius: 20px;
+    background: #0faf87;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+    
+}
+.block:hover{
+    box-shadow: 2px 2px 8px #0faf878a;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <p><b><i>Reaction Time: {{ score }} ms</i></b></p>
+        <p class="rank">{{ rank }}</p>
+    </div>
+</template>
+
+<script>
+    /* import { ref, reactive } from 'vue';*/
+
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null,
+        };
+    },
+    mounted() {
+      if (this.score < 350) {
+          this.rank = 'Damn!!! YOU ARE INCREDIBLY FAST'
+      } else if (this.score < 400) {
+          this.rank = 'You have rapid reflexes'
+      }else if (this.score < 500){
+          this.rank = 'You are slow....'
+      }else {
+          this.rank = 'YOU ARE INCREDIBLY SLOW'
+      }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style>
+.rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
+
+</style>
